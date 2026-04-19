6719ab7587e94b852d3c9a9040c9eed653e412be
Jeremy <jjlewis93@hotmail.com>
2026-04-19 13:18:34 -0600

    Page

--- devboard/src/app/page.tsx
+++ devboard/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div style={{ padding: "20px" }}>
-      Bienvenido a Dev Board, contamos con proyectos, tickts y un acerca de
+      Bienvenido a Dev Board, contamos con proyectos, tickets y un acerca de
     </div>
   );
 }
